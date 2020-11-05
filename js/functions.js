b5796b644be7bb2a2eb18a14eab4be6ec89d679a
Simone <simone.pagotto.kg@gmail.com>
2020-11-05 15:11:02 +0100

    logica e messaggio vittoria/sconfitta

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,20 @@ function nRandomIntNumbersToMax(length,min,max){
 }
 
 // function askLimitedNumbers()
+function askLimitedNumbers(length){
+  var array = [];
+  for (var i = 0; i < length; i++) {
+    array[i] = parseInt(prompt('Inserisci il numero: '));
+  }
+  return array;
+}
+
+function compareArraysByPosition(arrayOne,arrayTwo){
+  var booleanArray = [];
+  if(arrayOne.length === arrayTwo.length){
+    for (var i = 0; i < arrayOne.length; i++) {
+      booleanArray[i] = (arrayOne[i] === arrayTwo[i]);
+    }
+  }
+  return booleanArray;
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var randomNumbers = [];
+var userNumbers = [];
+var outcomeComparison = [];
 var N_RANDOM = 5;
 var N_MAX = 100;
 var N_MIN = 0;
+var hasWin = true;
 
 //genero 5 numeri random in un array;
 randomNumbers = nRandomIntNumbersToMax(N_RANDOM,N_MIN,N_MAX);
@@ -11,7 +14,26 @@ console.log(randomNumbers);
 alert(randomNumbers);
 
 //con la visualizzazione parte il timer
-setTimeout(askLimitedNumbers,5000);
+setTimeout(function(){
+  userNumbers = askLimitedNumbers(N_RANDOM);
+  console.log(userNumbers);
+  outcomeComparison = compareArraysByPosition(randomNumbers,userNumbers);
+  console.log(outcomeComparison);
 
-function askLimitedNumbers(N_RANDOM){
-  for (var i = 0; i < N_RANDOM; i++) {
+  for (var i = 0; i < outcomeComparison.length; i++) {
+    if(outcomeComparison[i] === false){
+      hasWin = false;
+    }
+  }
+
+  //comparo due array della medesima lunghezza e ricavo un array booleano
+  //su cui costruisco il messaggio di vittoria(sequenza e numeri necessari)
+
+  if(hasWin === true){
+    $('#text').text('VICTORY');
+  } else {
+    $('#text').text('LOSS');
+  }
+
+
+},1000);
